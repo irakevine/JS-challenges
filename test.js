c1d40f15be72ea0bedfd5a78af15252f2d33b804
irakevine <ikevine2016@gmail.com>
2023-08-08 20:49:29 +0200

    creating challenges files

---
+++ test.js
@@ -0,0 +1,87 @@
+// let s = 0
+//  const addition = function myAdd(i,p){
+//  s = i + p
+//  return s
+// } 
+
+// console.log(addition(2,8));
+
+///////////////////////////////////////
+
+// const biggNbr= function bigg(a,b){
+// if(a < b ){
+//     return a 
+// }
+//    return b
+// }
+// console.log(biggNbr(4,80))
+
+/////////////////////////////////////////////////
+
+
+// let i = 5; 
+//    function gret(n){
+// if(n > i ){
+//     console.log(n) 
+// }
+        
+//     }
+//     gret(0)
+
+///////////////////////////////////////////////////////
+// Write a function which adds two numbers and check if the sum of those numbers is greater than 20
+// let n= 0
+// let p=20
+// function sum(a,b){
+//     n = a+b
+//     if(n > p){
+//    console.log(n)
+//     }
+// }
+// sum(5,17)
+/////////////////////////////////////////////////////////
+//write function which test if a number is greater than 5 and less than to 10
+// let i= 5
+// let p= 10
+// function comp(n){
+//     if( n>i && n<10 ){
+//         console.log(n);
+//     }
+// }
+// comp(90)
+
+//////////////////////////////////////
+// Write a function which test the grade of a student in a certain subject. It displays ‘Fail’ if he is less than 49% and excellent if he is above or has 50%.
+
+// function grade(PH){
+//     if(PH <= 49){
+// console.log('fail'); 
+//     }else if(PH >= 50){
+// console.log('excellent'); 
+//     }
+// }
+// grade(49)
+
+// write a function which displays numbers from 1 to 10 
+// let i= 0;
+// function num(){
+// for(let n = 1; n <= 10; n++){
+//   i = n
+//   console.log(i)
+// }
+// }
+// num()
+////////////////////////////////////////////////////////////////////////////
+
+// write function which display even numbers from 1 to 20
+let n=0;
+function evn(){
+for(let i = 1; i<= 20; i++){
+    n=i
+    if(n %2== 0){
+
+    console.log(n);
+}
+    }
+}
+evn()
